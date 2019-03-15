794bd0716e82259376b16293348e1c97ec82cd2e
Olivier Demangeon <olivier.demangeon@lam.fr>
2019-03-15 11:14:27 +0100

    Add sentenv bash file.

---
+++ setenv_randomtools.bash
@@ -0,0 +1,8 @@
+# Set variables
+export RANDOMTOOLS=/Users/olivier/Softwares/random_tools/
+
+# Add random tools to the PYTHONPATH
+paths_append $RANDOMTOOLS PYTHONPATH
+
+# Set aliases
+alias cd_randomtools='cd $RANDOMTOOLS'
