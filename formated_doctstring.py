27e31c0a679aee71ec5b8ca34477f1daa3c29d2b
Olivier Demangeon <olivier.demangeon@lam.fr>
2019-03-15 10:55:53 +0100

    Add formated_docstring module.

---
+++ formated_doctstring.py
@@ -0,0 +1,30 @@
+""" Formating doctstrings
+"""
+
+
+def _formatDostring(*args, **kwargs):
+    """ Decorator to put before a function to be able to format it on import.
+
+    >>> from formated_docstring import _formatDostring
+    >>> @_formatDostring(__file__=__file__)
+    >>> def run_tests(args):
+    >>>     '''run tests on methods in {__file__}
+    >>>
+    >>>     usage: {__file__} --tests
+    >>>     '''
+    >>>     pass
+    """
+    def decorator(o):
+        o.__doc__ = o.__doc__.format(*args, **kwargs)
+        return o
+
+    return decorator
+
+# from
+# @_formatDostring(__file__=__file__)
+# def run_tests(args):
+#     """run tests on methods in {__file__}
+#
+#     usage: {__file__} --tests
+#     """
+#     pass
